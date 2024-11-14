1bbc11d2689602b22ba8169a958dc951869daa39
Adam <40431677@live.napier.ac.uk>
2024-11-14 16:38:36 +0000

    Added port forwarding. Can't test yet

--- Pipeline/Pipeline/App.xaml.cs
+++ Pipeline/Pipeline/App.xaml.cs
@@ -1,12 +1,77 @@
-﻿namespace Pipeline
+﻿using Microsoft.Extensions.Configuration;
+using Renci.SshNet;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pipeline
 {
     public partial class App : Application
     {
+
+        IConfigurationRoot config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        public class Remote()
+        {
+            public string Server { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+            public int Port { get; set; }
+        }
+
         public App()
         {
-            InitializeComponent();
+            Remote? remote = config.GetRequiredSection("Remote").Get<Remote>();
 
-            MainPage = new AppShell();
+            string server = remote.Server;
+            int port = remote.Port;
+            string username = remote.Username;
+            string password = remote.Password;
+
+
+
+            // Create a new SSH client connection
+            using (var client = new SshClient(server, port, username, password))
+            {
+                try
+                {
+                    client.Connect();
+
+                    if (client.IsConnected)
+                    {
+
+                        int localPort = 8080;
+                        string remoteHost = remote.Server;
+                        int remotePort = 22;
+
+                        // Create the port forwarding
+                        var portForwarded = new ForwardedPortLocal("127.0.0.1", (uint)localPort, remoteHost, (uint)remotePort);
+                        client.AddForwardedPort(portForwarded);
+                        portForwarded.Start();
+
+
+                        InitializeComponent();
+
+                        MainPage = new AppShell();
+
+                        portForwarded.Stop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("SSH client failed to connect.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+                finally
+                {
+                    client.Disconnect();
+                }
+            }
+
         }
     }
 }
